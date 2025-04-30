00900e2b59cb5145256d1c49da2a49ba99da90a1
IvanTusanin <ivantusanin@gmail.com>
2025-04-30 23:10:12 +0300

    ADD1

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 from pycparser.ply.lex import TOKEN
 
-TOKEN = ''
+TOKEN = '7741205242:AAEUsyMUyD-rfGCbTyhE33wduvlmH2sXSxY'
 
 THE_CAT_API_KEY = ("live_MkLXTHWFbGEDLEjytLnjufzEzbJ6eHuGywlXGemRi0YhpgM5hh6V5KuxZ60CXr0T")
+
+NASA_API_KEY = "J669ZA0oTzLiPNm5KpGSz5Nowp7o9Fwha8lSwgUS"
